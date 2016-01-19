bcf9f8d28f3888982fc5637ede40e060e2250cda
Guy Maliar <guy@docady.com>
2016-01-19 11:47:28 +0200

    go fmt

--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"strings"
 	"time"
-  "strings"
-  "fmt"
 
-  "os"
-  "os/signal"
-  "syscall"
+	"os"
+	"os/signal"
+	"syscall"
 
-  "encoding/binary"
-  "bytes"
+	"bytes"
+	"encoding/binary"
 
 	"github.com/boltdb/bolt"
 
@@ -35,17 +35,17 @@ var messages string = "messages"
 
 // Uint64ToByte converts an uint64 to bytes array in BigEndian
 func Uint64ToByte(data uint64) []byte {
-  buf := new(bytes.Buffer)
-  binary.Write(buf, binary.BigEndian, data)
-  return buf.Bytes()
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, data)
+	return buf.Bytes()
 }
 
 // ByteToUint64 converts an uint64 in bytes to int64 with BigEndian
 func ByteToUint64(data []byte) uint64 {
-  var value uint64
-  buf := bytes.NewReader(data)
-  binary.Read(buf, binary.BigEndian, &value)
-  return value
+	var value uint64
+	buf := bytes.NewReader(data)
+	binary.Read(buf, binary.BigEndian, &value)
+	return value
 }
 
 func init() {
@@ -53,7 +53,7 @@ func init() {
 }
 
 func main() {
-  var err error
+	var err error
 	db, err = bolt.Open("twitch.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
 
 	if err != nil {
@@ -72,44 +72,44 @@ func main() {
 		log.Fatal(err)
 	}
 
-  _, err = b.CreateBucketIfNotExists([]byte(users))
-  if err != nil {
-    log.Fatal(err)
-  }
+	_, err = b.CreateBucketIfNotExists([]byte(users))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  _, err = b.CreateBucketIfNotExists([]byte(messages))
-  if err != nil {
-    log.Fatal(err)
-  }
+	_, err = b.CreateBucketIfNotExists([]byte(messages))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if err = tx.Commit(); err != nil {
 		log.Fatal(err)
 	}
 
-  c := make(chan os.Signal, 1)
-  signal.Notify(c, os.Interrupt)
-  signal.Notify(c, syscall.SIGTERM)
-  go func() {
-      <-c
-      db.View(func(tx *bolt.Tx) error {
-          b := tx.Bucket([]byte(*channel))
-          bu := b.Bucket([]byte(users))
-          bm := b.Bucket([]byte(messages))
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	go func() {
+		<-c
+		db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(*channel))
+			bu := b.Bucket([]byte(users))
+			bm := b.Bucket([]byte(messages))
 
-          bu.ForEach(func(k, v []byte) error {
-              fmt.Printf("key=%s, value=%v\n", k, ByteToUint64(v))
-              return nil
-          })
+			bu.ForEach(func(k, v []byte) error {
+				fmt.Printf("key=%s, value=%v\n", k, ByteToUint64(v))
+				return nil
+			})
 
-          bm.ForEach(func(k, v []byte) error {
-              fmt.Printf("key=%s, value=%v\n", k, ByteToUint64(v))
-              return nil
-          })
+			bm.ForEach(func(k, v []byte) error {
+				fmt.Printf("key=%s, value=%v\n", k, ByteToUint64(v))
+				return nil
+			})
 
-          return nil
-      })
-      os.Exit(1)
-  }()
+			return nil
+		})
+		os.Exit(1)
+	}()
 
 	bot := ircx.WithLogin(*server, *name, *user, *password)
 	if err := bot.Connect(); err != nil {
@@ -146,54 +146,54 @@ func PingHandler(s ircx.Sender, m *irc.Message) {
 }
 
 func MsgHandler(s ircx.Sender, m *irc.Message) {
-  message := strings.Split(m.String(), " ")
-  user := strings.Split(message[0], "!")[0][1:]
+	message := strings.Split(m.String(), " ")
+	user := strings.Split(message[0], "!")[0][1:]
 
-  err := db.Update(func(tx *bolt.Tx) error {
-      words := make(map[string]uint64)
-      message[3] = message[3][1:]
+	err := db.Update(func(tx *bolt.Tx) error {
+		words := make(map[string]uint64)
+		message[3] = message[3][1:]
 
-      for _, element := range message[3:] {
-        words[element]++
-      }
+		for _, element := range message[3:] {
+			words[element]++
+		}
 
-      b := tx.Bucket([]byte(*channel))
-      bu := b.Bucket([]byte(users))
-      bm := b.Bucket([]byte(messages))
+		b := tx.Bucket([]byte(*channel))
+		bu := b.Bucket([]byte(users))
+		bm := b.Bucket([]byte(messages))
 
-      v := bu.Get([]byte(user))
-      var count uint64 = 0
-      if v != nil {
-        count = ByteToUint64(v)
-      }
+		v := bu.Get([]byte(user))
+		var count uint64 = 0
+		if v != nil {
+			count = ByteToUint64(v)
+		}
 
-      count = count + uint64(len(message[3:]))
-      err := bu.Put([]byte(user), Uint64ToByte(count))
-      if err != nil {
-        return err
-      }
+		count = count + uint64(len(message[3:]))
+		err := bu.Put([]byte(user), Uint64ToByte(count))
+		if err != nil {
+			return err
+		}
 
-      for word, count := range words {
-        v := bm.Get([]byte(word))
-        var sum uint64 = 0
+		for word, count := range words {
+			v := bm.Get([]byte(word))
+			var sum uint64 = 0
 
-        if v != nil {
-          sum = ByteToUint64(v)
-        }
+			if v != nil {
+				sum = ByteToUint64(v)
+			}
 
-        sum = sum + count
-        err := bm.Put([]byte(word), Uint64ToByte(sum))
-        if err != nil {
-          return err
-        }
-      }
+			sum = sum + count
+			err := bm.Put([]byte(word), Uint64ToByte(sum))
+			if err != nil {
+				return err
+			}
+		}
 
-      return nil
-  })
+		return nil
+	})
 
-  if err != nil {
-    log.Fatal(err)
-  } else {
-    log.Println(user + " said " + strings.Join(message[3:], " "))
-  }
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println(user + " said " + strings.Join(message[3:], " "))
+	}
 }
